6a1be6e525d30d49a1f6c07594db1b3262762be0
Kayu Choy <Pooh@Kayus-MBP.hsd1.pa.comcast.net>
2018-04-19 20:17:54 -0400

    Contains function model_select, which fits a linear regression of covariates with p-values less than or equal to a cutoff.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,15 @@ generate_data = function(n, p) {
   responses = rnorm(n, mean = 0, sd = 1)
   return(list(covariates, responses))
 }
+
+model_select = function(covariates, responses, cutoff) {
+  lm = lm(responses ~ covariates)
+  coefs = summary(lm)$Coefficients
+  pvals = coefs[3]
+  retain = covariates[, which(pvals <= cutoff)]
+  if (length(retain) == 0) {
+    return("")
+  } else {
+    return(lm(responses ~ retain))
+  }
+}
